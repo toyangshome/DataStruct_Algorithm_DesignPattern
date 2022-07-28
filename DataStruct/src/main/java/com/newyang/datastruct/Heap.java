e4f034d78544e3329e5334f8ff44f37f3ee1f1f1
newyang <1013836629@qq.com>
2022-07-29 00:14:48 +0800

    Add Heap and TreeNode

---
+++ DataStruct/src/main/java/com/newyang/datastruct/Heap.java
@@ -0,0 +1,15 @@
+package com.newyang.datastruct;
+
+import java.util.PriorityQueue;
+
+public class Heap {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+        priorityQueue.add(10);
+        priorityQueue.add(11);
+        priorityQueue.add(1);
+        priorityQueue.add(213);
+        priorityQueue.add(2);
+    }
+}
---
+++ DataStruct/src/main/java/com/newyang/datastruct/TreeNode.java
@@ -0,0 +1,17 @@
+package com.newyang.datastruct;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TreeNode {
+    TreeNode left;
+    TreeNode right;
+    Integer val;
+    public TreeNode(Integer val) {
+        this.val = val;
+    }
+
+    public static void main(String[] args) {
+        List<TreeNode> list = new ArrayList<>();
+    }
+}
